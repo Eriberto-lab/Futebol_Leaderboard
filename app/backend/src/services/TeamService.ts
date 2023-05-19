538fd8557d6a6db361bb51dceb23e727e53025d8
Eriberto Lima <eribertosflima@gmail.com>
2023-05-19 15:00:26 -0300

    feat: adiciona rota team e metodo getAll

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,10 +1,9 @@
-
-import TeamModel, { TeamAtributes } from "../database/models/Team.model";
+import TeamModel, { TeamAtributes } from '../database/models/Team.model';
 
 class TeamService {
-   public static async getAll(): Promise<TeamAtributes[]> {
-        return await  TeamModel.findAll()
-    }
+  public static async getAll(): Promise<TeamAtributes[]> {
+    return TeamModel.findAll();
+  }
 }
 
 export default TeamService;
