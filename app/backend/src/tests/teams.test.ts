3842ad93769bc97113230ab62477919d2da72678
Eriberto Lima <eribertosflima@gmail.com>
2023-05-21 14:04:40 -0300

    feat: adiciona test da rota /teams

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,21 +1,44 @@
 import * as Sinon from 'sinon';
 import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { app } from '../app';
+
+import { Response } from 'superagent';
 import TeamModel from "../database/models/Team.model"
 import TeamService from '../services/TeamService'
 import { TeamAtributes } from "../database/models/Team.model"
 import allTeams from '../mocks/mokedTeams'
 
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
 describe('Team Service', () => {
-    describe('getAll', () => {
+    afterEach(() => {
+        Sinon.restore();
+    })
+    describe('testa funçao getAll', () => {
         it('retorna um array com todos os times',async () => {
             // arrenge => dado um contexto
-           
             Sinon.stub(TeamModel, 'findAll').resolves(allTeams as TeamModel[])
             // act => açao
             const teams = await TeamService.getAll()
             // assert => oq é esperado
             chai.expect(teams).to.be.equal(allTeams)
-            Sinon.restore();
+            
+        })
+
+        it('testa rota /teams',async () => {
+            Sinon.stub(TeamModel, 'findAll').resolves(allTeams as TeamModel[])
+    
+            const teams = await chai.request(app).get('/teams');
+            
+            expect(teams.status).to.be.equal(200);
+            expect(teams.body).to.be.deep.equal(allTeams);
+    
         })
     })
+
+    
 })
